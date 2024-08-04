b680a04c22cd13d10d1f5af84a469e780a15b676
Birame S <birame.sene@zenika.com>
2024-08-04 06:29:22 +0200

    (big) refactoring

--- main.js
+++ main.js
@@ -1,73 +1,135 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
-canvas.style.border = "1px solid black";
+canvas.style.border = "3px solid black";
 canvas.width = 500;
 canvas.height = 500;
 
-const balle = { x: 250, y: 250, rayon: 20, couleur: "gold" };
-const direction = { x: Math.random() > 0.5 ? 1 : -1, y: 0 };
-const raquette = { largeur: 10, hauteur: 100 };
+class ArreterLeJeuEvenement extends Event {
 
-const raquetteGauche = {
-  ...raquette,
-  x: 25,
-  y: 300,
-  couleur: "red",
+  id;
+  constructor() {
+    super("arreterLeJeu");
+  }
+
+  action() {
+    clearInterval(this.id)
+  }
+}
+
+const arreterLeJeu = new ArreterLeJeuEvenement();
+
+document.addEventListener("arreterLeJeu", (e) => e.action())
+document.addEventListener("keydown", (e) => {
+  const { key } = e;
+
+  switch (key) {
+    case "ArrowDown":
+      raquetteGauche.moveDown(10)
+      break;
+    case "ArrowUp":
+      raquetteGauche.moveUp(10)
+      break;
+  }
+});
+
+class Raquette {
+  constructor({ x, y, largeur = 10, hauteur = 100, couleur }) {
+    this.x = x;
+    this.y = y;
+    this.largeur = largeur;
+    this.hauteur = hauteur;
+    this.couleur = couleur;
+  }
+  rendu(contexte) {
+    contexte.beginPath();
+    contexte.fillStyle = this.couleur;
+    contexte.rect(this.x, this.y, this.largeur, this.hauteur);
+    contexte.fill();
+  }
+
+  moveDown(y) {
+    this.y += y
+  }
+
+  moveUp(y) {
+    this.y -= y
+
+  }
+}
+
+class RaquetteGauche extends Raquette {
   toucheLeBord() {
     return balle.x < this.x + this.largeur + balle.rayon;
+  }
+}
+
+class RaquetteDroite extends Raquette {
+  toucheLeBord() {
+    return balle.x > this.x - balle.rayon;
+  }
+}
+
+const balle = {
+  x: 250, y: 250, rayon: 20, couleur: "gold",
+  toucheLeMur(zone) {
+    return this.x > zone.width - this.rayon || this.x < this.rayon;
   },
+  rendu(contexte) {
+    contexte.beginPath();
+    contexte.fillStyle = this.couleur;
+    contexte.ellipse(this.x, this.y, this.rayon, this.rayon, 0, 0, Math.PI * 2);
+    contexte.fill();
+  },
+  collision(raquette) {
+    const laBalleEstAuNiveauDeLaRaquette = (raquette) =>
+      this.y > raquette.y && this.y < raquette.y + raquette.hauteur;
+    return laBalleEstAuNiveauDeLaRaquette(raquette) && raquette.toucheLeBord();
+
+  },
+  move(direction) {
+    this.x += direction.x;
+    this.y += direction.y;
+  }
+};
+const direction = {
+  x: Math.random() > 0.5 ? 5 : -5, y: 0, reverseHorizontally() {
+    this.x = -this.x;
+  }
 };
 
-const raquetteDroite = {
-  ...raquette,
+const raquetteGauche = new RaquetteGauche({
+  x: 25,
+  y: 200,
+  couleur: "red",
+});
+
+const raquetteDroite = new RaquetteDroite({
   x: 465,
   y: 200,
   couleur: "royalblue",
-  toucheLeBord() {
-    return balle.x > this.x - balle.rayon;
-  },
-};
+});
 
-const laBalleEstAuNiveauDeLaRaquette = (raquette) =>
-  balle.y > raquette.y && balle.y < raquette.y + raquette.hauteur;
+const renduJeu = ({ balle, contexte, raquettes, zone }) => {
+  const [raquetteGauche, raquetteDroite] = raquettes;
+  contexte.reset();
+  balle.rendu(contexte);
+  raquetteDroite.rendu(contexte);
+  raquetteGauche.rendu(contexte);
 
-const collision = (raquette) =>
-  laBalleEstAuNiveauDeLaRaquette(raquette) && raquette.toucheLeBord();
+}
 
-const dessineRaquette = (raquette) => {
-  ctx.beginPath();
-  ctx.fillStyle = raquette.couleur;
-  ctx.rect(raquette.x, raquette.y, raquette.largeur, raquette.hauteur);
-  ctx.fill();
-};
 
-const dessineBalle = () => {
-  ctx.beginPath();
-  ctx.fillStyle = balle.couleur;
-  ctx.ellipse(balle.x, balle.y, balle.rayon, balle.rayon, 0, 0, Math.PI * 2);
-  ctx.fill();
-};
 
-const toucheLeMur = () =>
-  balle.x > canvas.width - balle.rayon || balle.x < balle.rayon;
-const boucleDeJeu = () => {
-  ctx.reset();
-  dessineBalle();
-  dessineRaquette(raquetteDroite);
-  dessineRaquette(raquetteGauche);
-  if (toucheLeMur()) {
-    clearInterval(animation);
-  }
-  if (collision(raquetteGauche) || collision(raquetteDroite)) {
-    direction.x = -direction.x;
+const animation = setInterval(() => {
+  if (balle.toucheLeMur(canvas)) {
+    arreterLeJeu.id = animation;
+    document.dispatchEvent(arreterLeJeu);
   }
 
-  if (balle.y > canvas.width - balle.rayon || balle.y < balle.rayon) {
-    direction.y = -direction.y;
+  if (balle.collision(raquetteGauche) || balle.collision(raquetteDroite)) {
+    direction.reverseHorizontally()
   }
-  balle.x += direction.x;
-  balle.y += direction.y;
-};
-const animation = setInterval(boucleDeJeu, 6);
+  renduJeu({ raquettes: [raquetteGauche, raquetteDroite], contexte: ctx, zone: canvas, balle })
 
-// const animation = setInterval(ball, 60 / 1000);
+  balle.move(direction)
+}, 10)
